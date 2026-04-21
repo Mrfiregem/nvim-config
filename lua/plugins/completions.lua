3b1da460ddcb40b5a329c957f235193a71bc658a
Firegem <firinggems@gmail.com>
2026-04-21 17:00:59 -0400

    use `mini.completion` instead of `cmp`

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,55 +1,17 @@
-local cmp = {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        -- Completion sources
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        -- Snippet source
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-    },
+local snippets = {
+    "nvim-mini/mini.snippets",
+    dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
-        local cmp = require("cmp")
-        local ls = require("luasnip")
-        cmp.setup {
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert {
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm { select = true },
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif ls.locally_jumpable(1) then
-                        ls.jump(1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif ls.locally_jumpable(-1) then
-                        ls.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            },
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-            }, { { name = "buffer" } }),
+        local gen_loader = require("mini.snippets").gen_loader
+        require("mini.snippets").setup {
+            snippets = { gen_loader.from_lang() },
         }
     end,
 }
 
-return cmp
+return {
+    "nvim-mini/mini.completion",
+    event = "BufRead",
+    dependencies = { snippets, "mini.icons" },
+    opts = {},
+}
