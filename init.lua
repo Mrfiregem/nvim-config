fa17c9185d07b3397edc77a1cded254fd02a4c8a
Firegem <firinggems@gmail.com>
2026-04-23 00:09:12 -0400

    move loading builtin commands after `lazy` init to prevent `undotree` errors

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
+-- Enable lazy.nvim plugin manager
+require("config.lazy")
+
 -- Enable builtin plugins
 local builtin_plugins = { "nohlsearch", "nvim.undotree" }
 for plugin in vim.iter(builtin_plugins) do
     vim.cmd.packadd(plugin)
 end
-
--- Enable lazy.nvim plugin manager
-require("config.lazy")
