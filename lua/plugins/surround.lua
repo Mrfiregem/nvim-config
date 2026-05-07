75601edbb7057d18129a487b283f37731b0f8cfc
Firegem <firinggems@gmail.com>
2026-05-07 05:12:28 -0400

    auto-insert closing delimiter when typing

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,1 +1,4 @@
-return { "nvim-mini/mini.surround", opts = {} }
+local surround = { "nvim-mini/mini.surround", opts = {} }
+local pairs = { "nvim-mini/mini.pairs", event = "InsertEnter", opts = {} }
+
+return { surround, pairs }
