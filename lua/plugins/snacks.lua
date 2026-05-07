ab692b391801b1cad001e3f42544a3a9bdfb0774
Firegem <firinggems@gmail.com>
2026-05-07 05:13:03 -0400

    add `snacks.nvim`

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,66 @@
+return {
+    "folke/snacks.nvim",
+    -- Load before other plugins
+    priority = 1000,
+    lazy = false,
+    ---@module 'snacks'
+    ---@type snacks.Config
+    opts = {
+        git = { enabled = true },
+        gh = { enabled = true },
+        lazygit = { enabled = true },
+        indent = { enabled = true },
+        scratch = { enabled = true },
+    },
+    keys = {
+        {
+            "<leader>.",
+            function()
+                Snacks.scratch()
+            end,
+            desc = "Toggle Scratch Buffer",
+        },
+        {
+            "<leader>S",
+            function()
+                Snacks.scratch.select()
+            end,
+            desc = "Select Scratch Buffer",
+        },
+        {
+            "<leader>gg",
+            function()
+                Snacks.lazygit()
+            end,
+            desc = "Lazygit",
+        },
+        {
+            "<leader>gi",
+            function()
+                Snacks.picker.gh_issue()
+            end,
+            desc = "Github Issues (open)",
+        },
+        {
+            "<leader>gI",
+            function()
+                Snacks.picker.gh_issue { state = "all" }
+            end,
+            desc = "Github Issues (all)",
+        },
+        {
+            "<leader>gi",
+            function()
+                Snacks.picker.gh_pr()
+            end,
+            desc = "Github Pull Requests (open)",
+        },
+        {
+            "<leader>gi",
+            function()
+                Snacks.picker.gh_pr { state = "all" }
+            end,
+            desc = "Github Pull Requests (all)",
+        },
+    },
+}
