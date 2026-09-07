0e41add7a529597d5c8fc45bb96d756d83eb870b
Firegem <5158194+Mrfiregem@users.noreply.github.com>
2026-09-07 14:25:12 -0400

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.expandtab = true
+opt.shiftwidth = 4
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
