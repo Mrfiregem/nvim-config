bf91717be82d5c09ddd41620a8cd97966c33e9e5
Firegem <5158194+Mrfiregem@users.noreply.github.com>
2026-09-10 14:30:56 -0400

    add `blink.cmp` package

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,14 +2,31 @@ local default_lsp_servers = {
     "lua_ls", "rust_analyzer"
 }
 
-return {
+local lspconfig =  {
     'mason-org/mason-lspconfig.nvim',
-    opts = {
-        ---@type string[]
-        ensure_installed = default_lsp_servers
-    },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
     },
+    opts = {
+        ensure_installed = default_lsp_servers
+    },
 }
+
+local lazydev = {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+        library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+    },
+}
+
+return {
+    lspconfig,
+    lazydev,
+}
+
