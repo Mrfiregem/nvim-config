b1ad1b5cd38553ec362c5e8dd41ed0da1d2fbb68
Firegem <5158194+Mrfiregem@users.noreply.github.com>
2026-09-11 09:10:02 -0400

    add `RenameFile` command
    renames the underlying filename of the current buffer on the system.

--- lua/config/util.lua
+++ lua/config/util.lua
@@ -7,4 +7,12 @@ vim.api.nvim_create_user_command("DeleteFile", function()
     else
         print("Could not delete file.")
     end
-end, {})
+end, { desc = "Delete the current buffer from system" })
+
+vim.api.nvim_create_user_command("RenameFile", function(cmdarg)
+    local path = vim.api.nvim_buf_get_name(0)
+    local new_name = cmdarg.fargs[1]
+    local new_path = vim.fs.joinpath(vim.fs.dirname(path), new_name)
+    vim.fs.rm(path)
+    vim.cmd.saveas(new_path)
+end, { nargs = 1, desc = "Rename the current buffer's filename" })
