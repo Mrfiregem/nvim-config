7e3307261eb3f1a20443a50218971f3162c39c56
Firegem <5158194+Mrfiregem@users.noreply.github.com>
2026-09-15 22:56:19 -0400

    improve `mini` helper function

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,13 @@
-local function mini(name, opts)
-    return {
-        string.format("nvim-mini/mini.%s", name),
-        version = false,
-        opts = opts or {},
-    }
+---@module "lazy.nvim"
+---@param name string The name of the mini package
+---@param o? LazyPluginSpec Other options
+---@return LazyPluginSpec
+local function mini(name, o)
+    local options = vim.tbl_deep_extend("force", { opts = {}, version = false }, o or {})
+    return vim.tbl_deep_extend("keep", { string.format("nvim-mini/mini.%s", name) }, options)
 end
 
 return {
-    mini("pairs"),
-    mini("surround"),
+    mini("pairs", { event = { "InsertEnter" } }),
+    mini("surround", { keys = { "sa", "sd", "sf", "sF", "sh", "sr" } }),
 }
